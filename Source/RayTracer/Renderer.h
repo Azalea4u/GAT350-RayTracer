56314844f7b2b087332b0670c00284c80d664d79
Dian Nguyen <dnguyen@student.neumont.edu>
2023-11-27 13:46:17 -0700

    11/27 update

--- Source/RayTracer/Renderer.h
+++ Source/RayTracer/Renderer.h
@@ -1,6 +1,25 @@
 #pragma once
 #include <string>
 #include <SDL.h>
+#include <glm/glm.hpp>
+
+struct Ray {
+    glm::vec3 origin;
+    glm::vec3 direction;
+
+    Ray() = default;
+    Ray(const glm::vec3& origin, const glm::vec3& direction)
+        : origin(origin), direction(glm::normalize(direction)) {}
+
+    glm::vec3 GetPoint(float distance) const {
+        return origin + distance * direction;
+    }
+
+    // Operator overload for multiplying a ray by a float
+    glm::vec3 operator*(float distance) const {
+        return GetPoint(distance);
+    }
+};
 
 class Renderer
 {
